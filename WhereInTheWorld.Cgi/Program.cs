672cbd8daf1776fe2108118fc3514727a0446746
Acidus <acidus99@users.noreply.github.com>
2022-08-01 08:06:29 -0400

    Convert to route handlers

--- WhereInTheWorld.Cgi/Program.cs
+++ WhereInTheWorld.Cgi/Program.cs
@@ -1,53 +1,17 @@
 ﻿using System;
 
-
 using Gemini.Cgi;
-using WhereInTheWorld.Models;
 
 namespace WhereInTheWorld.Cgi
 {
     class Program
     {
-        static GameEngine Engine;
-        static GameState State;
 
         static void Main(string[] args)
         {
-            using (var cgi = new CgiWrapper())
-            {
-                Engine = new GameEngine(cgi.ExecutingPath);
-                State = new GameState
-                {
-                    InputGuesses = ParseGuesses(cgi)
-                };
-                Engine.Play(State);
-
-                //now output results
-
-                Uri url = cgi.RequestUrl;
-                
-
-                cgi.Success();
-                GameRenderer renderer = new GameRenderer(cgi.Writer, Engine, GetGameUrl(cgi));
-                renderer.DrawState(State);
-            }
-        }
-
-        static string GetGameUrl(CgiWrapper cgi)
-            => $"gemini://{cgi.RequestUrl.Host}{cgi.RequestUrl.AbsolutePath}";
-
-        static List<string> ParseGuesses(CgiWrapper cgi)
-        {
-            if (cgi.HasQuery)
-            {
-                return cgi.Query
-                    .Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
-                    .Where(x => Engine.IsValidCountry(x))
-                    .Select(x => x.ToUpper())
-                    .Take(6)
-                    .ToList();
-            }
-            return new List<string>();
+            var cgiRouter = new CgiRouter();
+            cgiRouter.OnRequest("", RouteHandler.PlayGame);
+            cgiRouter.ProcessRequest();
         }
 
     }
---
+++ WhereInTheWorld.Cgi/RouteHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Gemini.Cgi;
+using WhereInTheWorld;
+namespace WhereInTheWorld.Cgi
+{
+	public static class RouteHandler
+	{
+
+		public static void PlayGame(CgiWrapper cgi)
+        {
+            var engine = new GameEngine(cgi.ExecutingPath);
+            var state = new GameState
+            {
+                InputGuesses = ParseGuesses(cgi, engine)
+            };
+            engine.Play(state);
+
+            cgi.Success();
+            GameRenderer renderer = new GameRenderer(cgi.Writer, engine, GetGameUrl(cgi));
+            renderer.DrawState(state);
+        }
+
+        static string GetGameUrl(CgiWrapper cgi)
+            => $"gemini://{cgi.RequestUrl.Host}{cgi.RequestUrl.AbsolutePath}";
+
+        static List<string> ParseGuesses(CgiWrapper cgi, GameEngine engine)
+        {
+            if (cgi.HasQuery)
+            {
+                return cgi.Query
+                    .Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+                    .Where(x => engine.IsValidCountry(x))
+                    .Select(x => x.ToUpper())
+                    .Take(6)
+                    .ToList();
+            }
+            return new List<string>();
+        }
+
+
+    }
+}
+
